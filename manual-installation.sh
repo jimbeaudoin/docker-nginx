fb4a5b9ea425b356884c8a885b8ce6ff9d525e6e
Jimmy Beaudoin <contact@jim-beaudoin.com>
2014-08-31 17:55:43 -0400

    Add manual-installation.sh file for manual installation.

---
+++ manual-installation.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Nginx 1.6.1 Installation on Debian 7.6
+
+# (c) Jimmy Beaudoin, 2014
+
+echo "Updating the system..."
+echo ""
+
+sudo apt-get update
+sudo apt-get -y upgrade
+
+echo "Installing prerequisites..."
+echo ""
+
+sudo apt-get -y install curl gcc make g++ openssl libssl-dev libpcre3-dev
+
+echo "Zlib installation..."
+echo ""
+
+curl -0 http://zlib.net/zlib-1.2.8.tar.gz
+tar -xzf zlib-1.2.8.tar.gz
+cd zlib-1.2.8
+./configure
+make && make install
+cd ..
+rm -rf zlib-1.2.8 zlib-1.2.8.tar.gz
+
+echo "Nginx installation..."
+echo ""
+
+curl -O http://nginx.org/download/nginx-1.6.1.tar.gz
+tar -xzf nginx-1.6.1.tar.gz
+cd nginx-1.6.1
+./configure --user=www-data --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-http_gzip_static_module --with-http_stub_status_module --with-http_ssl_module --with-pcre --with-file-aio --with-http_realip_module --without-http_scgi_module --without-http_uwsgi_module --without-http_fastcgi_module
+make && make install
+cd ..
+rm -rf nginx-1.6.1 nginx-1.6.1.tar.gz
+cat nginx > /etc/init.d/nginx
+
+echo "Cleaning up..."
+echo ""
+
+sudo apt-get -y remove curl gcc make g++
+sudo apt-get -y autoremove
+
+echo "Done!"
+echo "You can try 'service nginx start' or 'service nginx status' now."
