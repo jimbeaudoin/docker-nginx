03259cb8183ed9666cece09322ae19a097146d16
Jimmy Beaudoin <contact@jim-beaudoin.com>
2014-09-01 03:24:34 -0400

    Update commands.

--- manual-installation.sh
+++ manual-installation.sh
@@ -4,28 +4,33 @@
 
 # (c) Jimmy Beaudoin, 2014
 
+echo ""
 echo "Updating the system..."
 echo ""
 
 sudo apt-get update
 sudo apt-get -y upgrade
 
+echo ""
 echo "Installing prerequisites..."
 echo ""
 
 sudo apt-get -y install curl gcc make g++ openssl libssl-dev libpcre3-dev
 
+echo ""
 echo "Zlib installation..."
 echo ""
 
-curl -0 http://zlib.net/zlib-1.2.8.tar.gz
+curl -O http://zlib.net/zlib-1.2.8.tar.gz
 tar -xzf zlib-1.2.8.tar.gz
 cd zlib-1.2.8
 ./configure
-make && make install
+sudo make
+sudo make install
 cd ..
 rm -rf zlib-1.2.8 zlib-1.2.8.tar.gz
 
+echo ""
 echo "Nginx installation..."
 echo ""
 
@@ -33,16 +38,27 @@ curl -O http://nginx.org/download/nginx-1.6.1.tar.gz
 tar -xzf nginx-1.6.1.tar.gz
 cd nginx-1.6.1
 ./configure --user=www-data --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-http_gzip_static_module --with-http_stub_status_module --with-http_ssl_module --with-pcre --with-file-aio --with-http_realip_module --without-http_scgi_module --without-http_uwsgi_module --without-http_fastcgi_module
-make && make install
+sudo make
+sudo make install
 cd ..
 rm -rf nginx-1.6.1 nginx-1.6.1.tar.gz
-cat nginx > /etc/init.d/nginx
 
+echo ""
+echo "Copying Nginx script..."
+echo ""
+
+cat $PWD/nginx.script | sudo tee -a /etc/init.d/nginx >/dev/null
+
+sudo chmod +x /etc/init.d/nginx
+
+echo ""
 echo "Cleaning up..."
 echo ""
 
 sudo apt-get -y remove curl gcc make g++
 sudo apt-get -y autoremove
 
+echo ""
 echo "Done!"
 echo "You can try 'service nginx start' or 'service nginx status' now."
+echo ""
