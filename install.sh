69892e50511afed36554ffb3f2ec0198b5587089
Jimmy Beaudoin <jim@rockon.services>
2014-12-01 00:39:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -34,7 +34,7 @@ echo ""
 echo "Nginx installation..."
 echo ""
 
-curl -O http://nginx.org/download/nginx-1.6.1.tar.gz
+curl -O http://nginx.org/download/nginx-1.6.2.tar.gz
 tar -xzf nginx-1.6.1.tar.gz
 cd nginx-1.6.1
 ./configure --user=www-data --prefix=/etc/nginx --sbin-path=/usr/sbin/nginx --conf-path=/etc/nginx/nginx.conf --pid-path=/var/run/nginx.pid --lock-path=/var/run/nginx.lock --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-http_gzip_static_module --with-http_stub_status_module --with-http_ssl_module --with-pcre --with-file-aio --with-http_realip_module --with-http_spdy_module --without-http_scgi_module --without-http_uwsgi_module --without-http_fastcgi_module
